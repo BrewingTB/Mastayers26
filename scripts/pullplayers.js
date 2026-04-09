93e29266dda33e1205b7786e1c7ab6543f224a41
BrewingTB <tbrunetti@yahoo.com>
2026-04-09 09:10:33 -0400

    Update pullplayers.js

--- scripts/pullplayers.js
+++ scripts/pullplayers.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import https from "https";
 
-const API_KEY = process.env.SPORTSDATA_KEY; // stored in GitHub Secrets
+const API_KEY = process.env.GOLF_API_KEY;
 const url = `https://api.sportsdata.io/golf/v2/json/Players?key=${API_KEY}`;
 
 https.get(url, (res) => {
